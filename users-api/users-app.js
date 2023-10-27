000e0250cc5a8bbfec45b16dcd24a7430f0924d7
Lodenn <khalednasser788@gmail.com>
2023-10-27 13:13:30 +0200

    change trigger branches

--- users-api/users-app.js
+++ users-api/users-app.js
@@ -40,7 +40,7 @@ delay(7000)
       { useNewUrlParser: true },
       (err) => {
         if (err) {
-          console.log("COULD NOT CONNECT TO MONGODB!");
+          console.log("COULD NOT CONNECT TO MONGODB!: ", err);
           console.log(
             `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_DB_HOST}:27017/${process.env.MONGO_INITDB_DATABASE}`
           );

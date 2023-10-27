f98e67662be5699803f91450f29a9b76ff3a9517
Lodenn <khalednasser788@gmail.com>
2023-10-27 14:45:23 +0200

    update users image

--- users-api/users-app.js
+++ users-api/users-app.js
@@ -50,7 +50,7 @@ delay(7000)
           console.log(
             `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_DB_HOST}:27017/${process.env.MONGO_INITDB_DATABASE}`
           );
-          app.listen(3000);
+          app.listen(80);
         }
       }
     );

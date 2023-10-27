7fc6dbdf9664446d641c5ad7f600fb34e9df6c77
Lodenn <khalednasser788@gmail.com>
2023-10-27 17:06:08 +0200

    runs user container

--- auth-api/auth-app.js
+++ auth-api/auth-app.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const authRoutes = require('./routes/auth-routes');
+const authRoutes = require("./routes/auth-routes");
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
@@ -19,7 +19,7 @@ app.use(authRoutes);
 app.use((err, req, res, next) => {
   console.log(err);
   let code = 500;
-  let message = 'Something went wrong.';
+  let message = "Something went wrong.";
   if (err.code) {
     code = err.code;
   }
@@ -30,4 +30,6 @@ app.use((err, req, res, next) => {
   res.status(code).json({ message: message });
 });
 
-app.listen(3000);
+if (process.env.SHOULD_LISTEN) {
+  app.listen(3000);
+}

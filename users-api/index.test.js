1637e90602b04624d0365536d91624016ed6a344
Lodenn <khalednasser788@gmail.com>
2023-10-28 14:43:31 +0200

    add basic test case

--- users-api/index.test.js
+++ users-api/index.test.js
@@ -21,13 +21,13 @@ describe("insert", () => {
     await connection.close();
   });
 
-  //   it("should insert a doc into collection", async () => {
-  //     const users = db.collection("users");
+  it("should insert a doc into collection", async () => {
+    const users = db.collection("users");
 
-  //     const mockUser = { _id: "some-user-id", name: "John" };
-  //     await users.insertOne(mockUser);
+    const mockUser = { _id: "some-user-id", name: "John" };
+    await users.insertOne(mockUser);
 
-  //     const insertedUser = await users.findOne({ _id: "some-user-id" });
-  //     expect(insertedUser).toEqual(mockUser);
-  //   });
+    const insertedUser = await users.findOne({ _id: "some-user-id" });
+    expect(insertedUser).toEqual(mockUser);
+  });
 });

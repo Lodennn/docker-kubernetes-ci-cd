4f1818716fddec43c26a22c68052e3c239fc085f
Lodenn <khalednasser788@gmail.com>
2023-10-30 18:13:23 +0200

    add users

--- auth-api/auth-app.js
+++ auth-api/auth-app.js
@@ -20,7 +20,6 @@ app.use((err, req, res, next) => {
   console.log(err);
   let code = 500;
   let message = "Something went wrong.";
-  console.log("message: ", message);
   if (err.code) {
     code = err.code;
   }
--- users-api/users-app.js
+++ users-api/users-app.js
@@ -33,41 +33,21 @@ app.use((err, req, res, next) => {
 const delay = (miliseconds) =>
   new Promise((res) => setTimeout(res, miliseconds));
 
-// delay(7000)
-//   .then((_) => {
-//     mongoose.connect(
-//       `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_DB_HOST}:27017/${process.env.MONGO_INITDB_DATABASE}?authSource=admin`,
-//       { useNewUrlParser: true, useUnifiedTopology: true },
-//       (err) => {
-//         if (err) {
-//           console.log("ERROR: ", err);
-//           console.log("COULD NOT CONNECT TO MONGODB%%%%%%%%%%%%");
-//           console.log(
-//             `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_DB_HOST}:27017/${process.env.MONGO_INITDB_DATABASE}`
-//           );
-//         } else {
-//           console.log("DATABASE CONNECTED SUCCESSFULLY");
-//           console.log(
-//             `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_DB_HOST}:27017/${process.env.MONGO_INITDB_DATABASE}`
-//           );
-//           if (process.env.LISTEN) {
-//             app.listen(80);
-//           }
-//         }
-//       }
-//     );
-//   })
-//   .catch((err) => {
-//     console.log("DATABASE NOT CONNECTED");
-//   });
-
-mongoose.connect(
-  `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_DB_HOST}:27017/${process.env.MONGO_INITDB_DATABASE}?authSource=admin`,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err) => {
-    if (err) {
-    } else {
-      app.listen(80);
-    }
-  }
-);
+delay(7000)
+  .then((_) => {
+    mongoose.connect(
+      `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_DB_HOST}:27017/${process.env.MONGO_INITDB_DATABASE}?authSource=admin`,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      (err) => {
+        if (err) {
+          console.log("COULD NOT CONNECT TO MONGODB: ", err);
+        } else {
+          console.log("DATABASE CONNECTED SUCCESSFULLY");
+          app.listen(80);
+        }
+      }
+    );
+  })
+  .catch((err) => {
+    console.log("DATABASE NOT CONNECTED");
+  });

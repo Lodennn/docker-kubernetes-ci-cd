839fb4ccb1f704adb6a39e909d4b00c81596fded
Lodenn <khalednasser788@gmail.com>
2023-10-31 15:47:14 +0200

    update users image

--- users-api/users-app.js
+++ users-api/users-app.js
@@ -34,13 +34,13 @@ const delay = (miliseconds) =>
   new Promise((res) => setTimeout(res, miliseconds));
 
 console.log(
-  `mongodb+srv://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_DB_HOST}/${process.env.MONGO_INITDB_DATABASE}?authSource=admin`
+  `mongodb+srv://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_DB_HOST}/${process.env.MONGO_INITDB_DATABASE}?retryWrites=true&w=majority`
 );
 
 delay(7000)
   .then((_) => {
     mongoose.connect(
-      `mongodb+srv://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_DB_HOST}/${process.env.MONGO_INITDB_DATABASE}?authSource=admin`,
+      `mongodb+srv://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_DB_HOST}/${process.env.MONGO_INITDB_DATABASE}?retryWrites=true&w=majority`,
       { useNewUrlParser: true, useUnifiedTopology: true },
       (err) => {
         if (err) {

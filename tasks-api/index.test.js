a8b14e5240803062cdfecd762d94619fd955034b
Lodenn <khalednasser788@gmail.com>
2023-11-01 10:56:46 +0200

    update tasks test

--- tasks-api/index.test.js
+++ tasks-api/index.test.js
@@ -6,9 +6,6 @@ describe("insert", () => {
 
   beforeAll(async () => {
     try {
-      console.log(
-        `TASKS = mongodb+srv://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_DB_HOST}/${process.env.MONGO_INITDB_DATABASE}?retryWrites=true&w=majority`
-      );
       connection = await MongoClient.connect(
         `mongodb+srv://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_DB_HOST}/${process.env.MONGO_INITDB_DATABASE}?retryWrites=true&w=majority`,
         {
